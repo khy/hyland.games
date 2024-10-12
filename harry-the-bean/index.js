2fbb2f3d072f5cdb2a4b976938dfe0f1a5eeea19
walter0316 <kevin.john.hyland+harry@gmail.com>
2024-10-12 12:51:06 +0000

    Setup bean sprite

--- harry-the-bean/index.js
+++ harry-the-bean/index.js
@@ -1,3 +1,32 @@
 import kaplay from "kaplay";
 
-kaplay()
+kaplay();
+
+setGravity(2400);
+
+loadSprite("bean", "/bean.png");
+
+const bean = add([sprite("bean"), pos(80, 40), area(), body()]);
+
+add([
+  rect(1000, 50),
+  area(),
+  outline(4),
+  pos(300, 100),
+  body({
+    isStatic: true,
+  }),
+]);
+
+onKeyPress("space", () => {
+    bean.jump();
+  });
+  
+  onKeyDown("right", () => {
+    bean.move(100, 0);
+  });
+  
+  onKeyDown("left", () => {
+    bean.move(-100, 0);
+  });
+  
