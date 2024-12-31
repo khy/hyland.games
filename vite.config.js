091a89bed6ecd53b687b9667b62ae4e718df3e46
walter0316 <kevin.john.hyland+harry@gmail.com>
2024-12-31 17:36:31 +0000

    Tic Tac Toe

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         'harry-the-bean': resolve(__dirname, 'harry-the-bean/index.html'),
-        'the-legend-of-jack-destiny-rising': resolve(__dirname, 'the-legend-of-jack-destiny-rising/index.html')
+        'the-legend-of-jack-destiny-rising': resolve(__dirname, 'the-legend-of-jack-destiny-rising/index.html'),
+        'tic-tac-toe': resolve(__dirname, 'tic-tac-toe/index.html')
       }
     }
   }
