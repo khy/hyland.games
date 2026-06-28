682eb5e83d741f7cb75bbad1d168972371f24f82
Jack Hyland <kevin.john.hyland+jack@gmail.com>
2026-06-27 20:33:59 -0400

    Added jacky bird!

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,24 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        'harry-the-bean': resolve(__dirname, 'harry-the-bean/index.html'),
-        'the-legend-of-jack-destiny-rising': resolve(__dirname, 'the-legend-of-jack-destiny-rising/index.html'),
-        'tic-tac-toe': resolve(__dirname, 'tic-tac-toe/index.html'),
-        'jake': resolve(__dirname, 'jake/index.html'),
-        'pixel-kingdom': resolve(__dirname, 'pixel-kingdom/index.html'),
-        'text-quest': resolve(__dirname, 'text-quest/index.html')
-      }
-    }
-  }
-})
+        main: resolve(__dirname, "index.html"),
+        "harry-the-bean": resolve(__dirname, "harry-the-bean/index.html"),
+        "the-legend-of-jack-destiny-rising": resolve(
+          __dirname,
+          "the-legend-of-jack-destiny-rising/index.html",
+        ),
+        "tic-tac-toe": resolve(__dirname, "tic-tac-toe/index.html"),
+        jake: resolve(__dirname, "jake/index.html"),
+        "pixel-kingdom": resolve(__dirname, "pixel-kingdom/index.html"),
+        "text-quest": resolve(__dirname, "text-quest/index.html"),
+        "jacky-bird": resolve(__dirname, "jacky-bird/index.html"),
+      },
+    },
+  },
+});
