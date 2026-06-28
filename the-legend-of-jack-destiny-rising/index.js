d33fe51148dade9347823c5ff8b486bd48dda9e7
Jack Hyland <kevin.john.hyland+jack@gmail.com>
2026-06-27 20:23:53 -0400

    Formatting

--- the-legend-of-jack-destiny-rising/index.js
+++ the-legend-of-jack-destiny-rising/index.js
@@ -7,54 +7,54 @@ const WindowColumns = 18;
 const WindowRows = 10;
 
 kaplay({
-    width: WindowColumns * TileWidth,
-    height: WindowRows * TileHeight
+  width: WindowColumns * TileWidth,
+  height: WindowRows * TileHeight,
 });
 
 loadSprite("grassTiles", "/assets/Grass_tiles_v2.png", {
-    sliceX: 11,
-    sliceY: 7
+  sliceX: 11,
+  sliceY: 7,
 });
 
 loadSprite("waterTiles", "/assets/Water.png", {
-    sliceX: 4,
-    sliceY: 1,
-    anims: {
-        main: { from: 0, to: 3, loop: true}
-    }
-})
+  sliceX: 4,
+  sliceY: 1,
+  anims: {
+    main: { from: 0, to: 3, loop: true },
+  },
+});
 
 loadSprite("jack", "/assets/Premium Charakter Spritesheet.png", {
-    sliceX: 8,
-    sliceY: 24,
-    anims: {
-        idleDown: { from: 0, to: 7, loop: true },
-        idleUp: { from: 8, to: 15, loop: true },
-        idleRight: { from: 16, to: 23, loop: true },
-        idleLeft: { from: 24, to: 31, loop: true },
-        walkDown: { from: 32, to: 39, loop: true },
-        walkUp: { from: 40, to: 47, loop: true },
-        walkRight: { from: 48, to: 55, loop: true },
-        walkLeft: { from: 56, to: 63, loop: true },
-    }
-})
+  sliceX: 8,
+  sliceY: 24,
+  anims: {
+    idleDown: { from: 0, to: 7, loop: true },
+    idleUp: { from: 8, to: 15, loop: true },
+    idleRight: { from: 16, to: 23, loop: true },
+    idleLeft: { from: 24, to: 31, loop: true },
+    walkDown: { from: 32, to: 39, loop: true },
+    walkUp: { from: 40, to: 47, loop: true },
+    walkRight: { from: 48, to: 55, loop: true },
+    walkLeft: { from: 56, to: 63, loop: true },
+  },
+});
 
 function setTile(column, row, spriteType, spriteFrame) {
-    add([
-        sprite(spriteType, { frame: spriteFrame }),
-        pos(column * TileWidth, row * TileHeight),
-        scale(SpriteScale)
-    ])
+  add([
+    sprite(spriteType, { frame: spriteFrame }),
+    pos(column * TileWidth, row * TileHeight),
+    scale(SpriteScale),
+  ]);
 }
 
 for (let column = 0; column < WindowColumns; column++) {
-    for (let row = 0; row < WindowRows; row++) {
-        add([
-            sprite("waterTiles", { anim: 'main' }),
-            pos(column * TileWidth, row * TileHeight),
-            scale(SpriteScale)
-        ]);
-    }
+  for (let row = 0; row < WindowRows; row++) {
+    add([
+      sprite("waterTiles", { anim: "main" }),
+      pos(column * TileWidth, row * TileHeight),
+      scale(SpriteScale),
+    ]);
+  }
 }
 
 setTile(0, 0, "grassTiles", 0);
@@ -72,7 +72,7 @@ setTile(9, 5, "grassTiles", 56);
 setTile(10, 5, "grassTiles", 58);
 setTile(11, 5, "grassTiles", 56);
 setTile(12, 5, "grassTiles", 56);
-setTile(13, 5, "grassTiles", 56)
+setTile(13, 5, "grassTiles", 56);
 setTile(14, 5, "grassTiles", 56);
 setTile(4, 5, "grassTiles", 56);
 setTile(3, 5, "grassTiles", 11);
@@ -99,64 +99,64 @@ setTile(3, 4, "grassTiles", 9);
 setTile(5, 4, "grassTiles", 28);
 
 const jack = add([
-    sprite("jack", { anim: "idleDown" }),
-    scale(SpriteScale),
-    pos(0, 0),
-    area(),
-    body(),
-    {
-        isWallking: false
-    }
+  sprite("jack", { anim: "idleDown" }),
+  scale(SpriteScale),
+  pos(0, 0),
+  area(),
+  body(),
+  {
+    isWallking: false,
+  },
 ]);
 
 onKeyDown("right", () => {
-    if (!jack.isWalking) {
-        jack.isWalking = true
-        jack.play("walkRight");
-    }
-    jack.move(100, 0);
+  if (!jack.isWalking) {
+    jack.isWalking = true;
+    jack.play("walkRight");
+  }
+  jack.move(100, 0);
 });
 
 onKeyRelease("right", () => {
-    jack.isWalking = false
-    jack.play("idleRight")
+  jack.isWalking = false;
+  jack.play("idleRight");
 });
 
 onKeyDown("left", () => {
-    if (!jack.isWalking) {
-        jack.isWalking = true
-        jack.play("walkLeft")
-    }
-    jack.move(-100, 0);
+  if (!jack.isWalking) {
+    jack.isWalking = true;
+    jack.play("walkLeft");
+  }
+  jack.move(-100, 0);
 });
 
 onKeyRelease("left", () => {
-    jack.isWalking = false
-    jack.play("idleLeft")
+  jack.isWalking = false;
+  jack.play("idleLeft");
 });
 
 onKeyDown("up", () => {
-    if (!jack.isWalking) {
-        jack.isWalking = true
-        jack.play("walkUp")
-    }
-    jack.move(0, -100);
+  if (!jack.isWalking) {
+    jack.isWalking = true;
+    jack.play("walkUp");
+  }
+  jack.move(0, -100);
 });
 
 onKeyRelease("up", () => {
-    jack.isWalking = false
-    jack.play("idleUp")
+  jack.isWalking = false;
+  jack.play("idleUp");
 });
 
 onKeyDown("down", () => {
-    if (!jack.isWalking) {
-        jack.isWalking = true
-        jack.play("walkDown")
-    }
-    jack.move(0, 100);
+  if (!jack.isWalking) {
+    jack.isWalking = true;
+    jack.play("walkDown");
+  }
+  jack.move(0, 100);
 });
 
 onKeyRelease("down", () => {
-    jack.isWalking = false
-    jack.play("idleDown")
+  jack.isWalking = false;
+  jack.play("idleDown");
 });
